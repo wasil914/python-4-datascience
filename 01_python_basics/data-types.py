371b7f362a1126a6367ba9bac6f54838f18ee1fb
wasil914 <wasilkhan914@gmail.com>
2026-06-20 19:50:51 +0530

    basics- print(),variables, chaining , datatypes

---
+++ 01_python_basics/data-types.py
@@ -0,0 +1,24 @@
+a = 10
+print(a,type(a))
+
+a = 0
+print(a,type(a))
+
+a = -123
+print(a,type(a))
+
+f = 12.3
+print(f,type(f))
+
+b = True
+print(b,type(b))
+
+st = 'waz'
+print(st,type(st))
+
+cplx = 2 + 4j
+print(cplx,type(cplx))
+
+j = complex(3,4)
+print(j)
+
---
+++ 01_python_basics/variable-in-python.py
@@ -0,0 +1,67 @@
+s_str = "kodnest"
+print(s_str)
+
+a = 10
+b = 20
+print(a+b)
+print(a,b)
+
+a= 40
+print(a+b)
+
+a = 4.8
+print(a+b)
+
+c= "10"
+d = 10
+print(c+str(d))
+
+A = 10
+a = 10
+print(a+b)
+
+x=y=z=100
+print(x+y+z)
+print(x,y,z)
+
+# Rules for naming variables in python
+
+my_variable1 = 100
+MY_VARIABLE2 = 200
+_var = 4
+print(_var)
+
+a,b,c = 10,40,50
+print(a,b,c)
+print(a+b+c)
+
+age = 23
+print(age)
+age = age + 2
+print(age)
+
+#mcq
+num1 = 5
+num2 = 10
+num1 , num2 = num2 , num1
+print(num1,num2)
+
+a   =   b   =    c   =  0
+b = 5
+print(a,b,c)
+
+price = 100
+price = price + 50
+print(price)
+
+a = 5
+b = a
+a = 10
+print(a,b) # 10 5
+
+x = "10"
+y = 5
+print(x*y)
+
+s = "aba" * 5
+print(s)
