342ae7267762d9340ba8902a7c1391cc10f056ea
wasil914 <wasilkhan914@gmail.com>
2026-06-20 11:58:53 +0530

    Basics-day-01

---
+++ 01_python_basics/IntroToPython.py
@@ -0,0 +1,25 @@
+# Single line Comment
+
+''' Multiline
+--------------
+----Comment'''
+
+# Features of python :
+'''
+Python is a programming language just like C , C++ , Java , Javascript , C# , PHP etc.
+
+It is special because its little different from other programming languages.
+
+Some of the strong features of python that makes it stand out from other programming languages are :
+
+1.Simple and easy to learn syntax.
+2.Python is a scripting language.
+3.Python is an interpreted language(*Hybrid).
+4.Python is a dynamically typed language.
+5.Python is a general purpose programming language.
+6.Python is data centric programming language.
+7.Python is platform independent programming language and portable.
+8.It supports Procedural , Modular , Object Oriented and Functional programming paradigms.
+and much more
+
+'''
---
+++ 01_python_basics/basics_d1.py
@@ -0,0 +1,54 @@
+print("Data"); print("Analyst")
+
+
+print("Data",end="")
+print("Analyst")
+
+print("Python",end="\n")
+print("developer")
+
+print("PowerBI",end=" \t ")
+print("developer")
+
+print("14","09","2002",sep="/")
+print("14","09","2002",sep="\t")
+
+print("code","debug","repeat" , sep="\n")
+print("miss","is","sippi",sep="\n")
+
+
+name = "Guido van Rossum"
+age = 48
+
+print("My name is "+ name +", and my age is ",age) # comma automatically adds space and converts age to string
+print("OR")
+print("My name is "+ name +", and my age is "+ str(age)) # Typecase , we have to convert age to string using str() function to concatenate it with other strings
+
+var_a = 12
+var_b = 138
+
+print(var_a);print(var_b)
+
+print(var_a,end=' ')
+print(var_b)
+
+print(var_a,var_b)
+
+print(var_a + var_b)
+
+var_c = '12'
+var_d = 138
+print(var_a);print(var_b)
+
+print(var_c,end=' ')
+print(var_d)
+
+print(var_c,var_d)
+
+# print(var_c + var_d) /// error because var_c is string and var_d is integer and we cannot add string and integer together without typecasting
+
+print(type(var_c))
+print(type(var_a))
+
+print("A man \na plan \na canal Panama")
+print("apple banana \tapple mango \napple")
