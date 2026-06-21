db6a846eae341f7c8d33e47a00bde44840d7cf4c
wasil914 <wasilkhan914@gmail.com>
2026-06-21 23:10:57 +0530

    Introduction to String basics , Slicing

---
+++ 01_python_basics/strings-in-py.py
@@ -0,0 +1,52 @@
+str = 'Hello'
+print(str)
+
+str1 = "Helloo"
+print(str1)
+
+# triple quotes are used for multi-line string and comment
+str2 = '''Hello-mlinestring'''
+str3 = """mlinestring"""
+print(str2,str3)
+
+
+str_m = '''hi 
+hello 
+wazup
+multiline string 
+assignment'''
+print(str_m)
+
+str_sq = ''' triple single
+quotes
+hello 
+wazup
+multiline string 
+assignment'''
+print(str_sq)
+
+"""triple quotes single or double are used for multiline comments and if assigned to a variable they are multiline strings"""
+
+# str_u= " checking if s
+# ingle double 
+# quote will show"
+#error
+
+str = "I'm Gamana"
+print(str)
+
+str_new = "I'm Gamana from \"Banglore\" "
+print(str_new)
+
+#Escape sequence characters
+str_s = '''Hello?
+I\'m Anushka, 
+from \"Kodnest\"
+How\'s everyryone '''
+print(str_s)
+
+st = "hello"\
+"fine"\
+"thanks"\
+
+print(st)
---
+++ 04_strings/string_basics.py
@@ -0,0 +1,52 @@
+str= "Hey"\
+"Hello"\
+"What's up"
+print(str)
+
+str = 'This is a String- with single quotes.'
+print(str)
+print("\n")
+
+str = "This is a string- with double quotes. "
+print(str)
+print("\n")
+
+str = '''This is 
+a multiline string 
+which uses 
+three single quotes.'''
+print(str)
+print("\n")
+
+
+str = """This is  a
+multi-line 
+string
+that uses
+three double quotes.
+ """
+print(str)
+
+"""
+Multiline comment
+ i.e 
+text enclosed in triple quotes (single /double) 
+if assigned to a variable acts as multiline string
+"""
+#OR
+"""triple quotes single or double are used for multiline comments and if assigned to a variable they are multiline strings"""
+
+
+#Escape Sequence Characters
+str = "I'm Wasil."
+print(str)
+
+str = '''I'm Wasil, from "Kashmir"'''
+print(str)
+
+#correct way
+str = 'I\'m Wasil'
+print(str)
+
+str = 'I\'m Wasil, from \"Kashmir\"'
+print(str)
