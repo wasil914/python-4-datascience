abc80a9c510ad1dcb8be3768299ccfa3325ed11c
wasil914 <wasilkhan914@gmail.com>
2026-07-01 19:07:06 +0530

    more on lists - slicing

---
+++ 05_lists/list-slicing.py
@@ -0,0 +1,18 @@
+nums = [10,20,30,40,50,60,70,80,90]
+print(nums[3:7])
+print(nums[-5:])
+print(nums[1:-1])
+print(nums[2::3])
+print(nums[::2])
+print(nums[::-1])
+print(nums[7:1:-1])
+print(nums[8:2:-2])
+print(nums[:4:2])
+
+
+
+
+
+li_st =['ash','bharat','chand','divy','gamana','harsh']
+print(li_st[::2])
+
